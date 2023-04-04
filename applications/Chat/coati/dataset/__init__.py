629102fabf82dcf8e3eb9610c25a0cfde149562d
Robert Myers <carro@DESKTOP-EOS3AJ0.localdomain>
2023-04-04 12:33:29 -0500

    update

--- applications/Chat/coati/dataset/__init__.py
+++ applications/Chat/coati/dataset/__init__.py
@@ -1,5 +1,5 @@
 from .prompt_dataset import PromptDataset
-from .reward_dataset import HhRlhfDataset, RmStaticDataset
+from .reward_dataset import HhRlhfDataset, RmStaticDataset, SHPDataset
 from .sft_dataset import DataCollatorForSupervisedDataset, SFTDataset, SupervisedDataset
 from .utils import is_rank_0
 
