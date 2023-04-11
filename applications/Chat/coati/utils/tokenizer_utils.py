7e4dd50f09b144249934cb86512dce578873c59a
Robert Myers <carro@DESKTOP-EOS3AJ0.localdomain>
2023-04-11 13:41:00 -0500

    update

--- applications/Chat/coati/utils/tokenizer_utils.py
+++ applications/Chat/coati/utils/tokenizer_utils.py
@@ -69,7 +69,7 @@ def smart_tokenizer_and_embedding_resize(
 
         if num_new_tokens > 0:
             input_embeddings = model.model.get_input_embeddings().weight.data
-            output_embeddings = model.model.get_output_embeddings().weight.data
+            output_embeddings = model.get_output_embeddings().weight.data
 
             input_embeddings_avg = input_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
             output_embeddings_avg = output_embeddings[:-num_new_tokens].mean(dim=0, keepdim=True)
