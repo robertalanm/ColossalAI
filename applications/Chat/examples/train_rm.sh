09f1983263ef565a72d97ef132b9b89f4901f6ec
Robert Myers <carro@DESKTOP-EOS3AJ0.localdomain>
2023-04-11 13:36:30 -0500

    fix

--- applications/Chat/examples/train_rm.sh
+++ applications/Chat/examples/train_rm.sh
@@ -1,8 +1,8 @@
 set_n_least_used_CUDA_VISIBLE_DEVICES 1
 
-python train_reward_model.py --pretrain 'microsoft/deberta-v3-large' \
-                             --model 'deberta' \
+python train_reward_model.py --pretrain '/home/ubuntu/hf_models/7b' \
+                             --model 'llama' \
                              --strategy naive \
                              --loss_fn 'log_exp'\
-                             --save_path 'rmstatic.pt' \
+                             --save_path 'bpt-rm-7b.pt' \
                              --test True
